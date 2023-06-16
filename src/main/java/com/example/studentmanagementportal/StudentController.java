3748c8c2beee03e1b383f965f4d53259ea9070b2
sachin jadhao <jadhaosachin7171@gmail.com>
2023-06-16 15:27:59 +0530

    Response Entity added

--- src/main/java/com/example/studentmanagementportal/StudentController.java
+++ src/main/java/com/example/studentmanagementportal/StudentController.java
@@ -1,6 +1,9 @@
 package com.example.studentmanagementportal;
 
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -16,40 +19,66 @@ public class StudentController {
 //    getting info of studetn by id
 
     @GetMapping("/get_info")
-    public Student getStudent(@RequestParam("id") int admissionNo){
-        return studentService.getStudent(admissionNo);
+    public ResponseEntity getStudent(@RequestParam("id") int admissionNo){
+        Student s = studentService.getStudent(admissionNo);
+
+        if(s==null){
+            return new ResponseEntity("student is not found", HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity(s,HttpStatus.ACCEPTED);
     }
 
 //    add the student to our db and this is post API and we are sending object so request body
     @PostMapping("/add")
-    public String addStudent(@RequestBody Student student){
-        return studentService.addStudent(student);
+    public ResponseEntity addStudent(@RequestBody Student student){
+        String s = studentService.addStudent(student);
+
+       return new ResponseEntity(s,HttpStatus.CREATED);
     }
 
 //    getting info by pathvariable
     @GetMapping("/get/{id}/{messege}")
-    public Student getStudent(@PathVariable("id") int admissionNo,@PathVariable("messege") String message){
-       return studentService.getStudent(admissionNo,message);
+    public ResponseEntity getStudent(@PathVariable("id") int admissionNo,@PathVariable("messege") String message){
+       Student s = studentService.getStudent(admissionNo,message);
+
+       if(s==null){
+           return new ResponseEntity("Student Not found",HttpStatus.NOT_FOUND);
+       }
+       return new ResponseEntity(s,HttpStatus.ACCEPTED);
 
     }
 
 //    delete student
     @DeleteMapping("/delete")
-    public String deleteStudent(@RequestParam("id") int admissionNo){
-       return studentService.deleteStudent(admissionNo);
+    public ResponseEntity deleteStudent(@RequestParam("id") int admissionNo){
+       String s = studentService.deleteStudent(admissionNo);
+
+       return new ResponseEntity(s,HttpStatus.ACCEPTED);
     }
 
 //  update the course of student
     @PutMapping("/update")
-    public Student changeCourse(@RequestParam("id") int admissionNo,@RequestParam("course") String course){
-       return studentService.changeCourse(admissionNo,course);
+    public ResponseEntity changeCourse(@RequestParam("id") int admissionNo,@RequestParam("course") String course){
+       Student s =  studentService.changeCourse(admissionNo,course);
+
+       if(s==null){
+           return new ResponseEntity("student not found",HttpStatus.NOT_FOUND);
+       }
+
+       return new ResponseEntity(s,HttpStatus.ACCEPTED);
     }
 
 
 //  find all the student whose age is greater than 25
     @GetMapping("/getTotal")
-    public int getTotalStudent(){
-        return studentService.getTotalStudent();
+    public ResponseEntity getTotalStudent(){
+        int total =  studentService.getTotalStudent();
+
+        if(total <= 0){
+            return new ResponseEntity("No students are greater than 25",HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity(total,HttpStatus.ACCEPTED);
     }
 
 }
